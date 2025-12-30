c13a042166eeff4717d3498352317a070887d1e5
Archlord12345 <ravel.nghomsi@facsciences-uy1.cm>
2025-12-30 21:04:56 +0100

    vehicule pro

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -8,7 +8,7 @@ const projects = [
     { id: 2, category: 'Gadgets', title: 'T-Shirts Personnalisés', image: 'https://images.unsplash.com/photo-1529374255404-311a2a4f1fd9?q=80&w=600&auto=format&fit=crop' },
     { id: 3, category: 'Infographie', title: 'Création Logo', image: 'https://images.unsplash.com/photo-1626785774625-ddcddc3445e9?q=80&w=600&auto=format&fit=crop' },
     { id: 4, category: 'Gadgets', title: 'Mugs Magiques', image: 'https://live.staticflickr.com/65535/55014363461_7ce5a00f31.jpg' },
-    { id: 5, category: 'Grand Format', title: 'Habillage Véhicule', image: 'https://images.unsplash.com/photo-1617727553252-65863c156eb0?q=80&w=600&auto=format&fit=crop' },
+    { id: 5, category: 'Grand Format', title: 'Habillage Véhicule', image: 'https://live.staticflickr.com/65535/55014679925_c72cda9c62_w.jpg' },
     { id: 6, category: 'Infographie', title: 'Flyer Event', image: 'https://static.vecteezy.com/ti/vecteur-libre/p1/1829863-business-design-concept-poster-with-flat-cartoon-illustration-flyer-business-pamphlet-brochure-magazine-cover-design-layout-space-for-promotion-advertising-marketing-vector-print-template-in-taille-a4-gratuit-vectoriel.jpg' },
 ];
 
