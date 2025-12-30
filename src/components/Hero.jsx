00698f9e474e40c0eed71199f65d1de586079e24
Archlord12345 <ravel.nghomsi@facsciences-uy1.cm>
2025-12-30 19:32:35 +0100

    test2

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -8,8 +8,9 @@ const Hero = () => {
             <div className="hero-bg"></div>
             <div className="container hero-container">
                 <div className="hero-content">
+                    <br /><br />
                     <div className="hero-badge">
-                        <br /><br />
+
                         <span className="scrolling-text">Impression Numérique • Infographie • Bureautique</span>
                     </div>
                     <h1>
